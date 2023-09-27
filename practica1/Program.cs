c949b946b33e31c72739c8df3b593308541ed37e
dougl <dougl@LAPTOP-BCS0DN23>
2023-09-26 21:11:02 -0600

    Agregar archivos de proyecto.

---
+++ practica1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Profe,  Alexander Benjamin Curling");
+            Console.ReadLine();
+        }
+    }
+}
